79fa9a3074943f35a3d24991292ea31a8b6047cf
jacobrugano <jacob.rugano@student.moringaschool.com>
2021-12-20 01:07:09 +0300

    Added jquery disappearing effects onto the fortfolio cards

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,40 @@ $('#icon1').mouseenter(function () {
 }).mouseleave(function () {
     $('#icon1-title').hide("slow");
 });
+$('#icon2').mouseenter(function () {
+    $('.icon-heading1').filter('#icon2-title').show("slow");
+}).mouseleave(function () {
+    $('#icon2-title').hide("slow");
+});
+
+$('#icon3').mouseenter(function () {
+    $('.icon-heading2').filter('#icon3-title').show("slow");
+}).mouseleave(function () {
+    $('#icon3-title').hide("slow");
+});
+
+$('#icon4').mouseenter(function () {
+    $('.icon-heading3').filter('#icon4-title').show("slow");
+}).mouseleave(function () {
+    $('#icon4-title').hide("slow");
+});
+$('#icon5').mouseenter(function () {
+    $('.icon-heading4').filter('#icon5-title').show("slow");
+}).mouseleave(function () {
+    $('#icon5-title').hide("slow");
+});
+$('#icon6').mouseenter(function () {
+    $('.icon-heading5').filter('#icon6-title').show("slow");
+}).mouseleave(function () {
+    $('#icon6-title').hide("slow");
+});
+$('#icon7').mouseenter(function () {
+    $('.icon-heading6').filter('#icon7-title').show("slow");
+}).mouseleave(function () {
+    $('#icon7-title').hide("slow");
+});
+$('#icon8').mouseenter(function () {
+    $('.icon-heading7').filter('#icon8-title').show("slow");
+}).mouseleave(function () {
+    $('#icon8-title').hide("slow");
+});
