711c2862dc7c1c1770e74b5e542e3d485bb48d4b
jacobrugano <jacob.rugano@student.moringaschool.com>
2022-01-06 11:47:37 +0300

    updating jquery on contacts page

--- js/scripts.js
+++ js/scripts.js
@@ -75,20 +75,34 @@ $('#devlpmt').click(function () {
       });
   });
 
-  $("form#contactscontainer").on('submit',function(event){
-            event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
+  // $("form#contactscontainer").on('submit',function(event){
+  //           event.preventDefault();
+  //           var name = $("input#name").val();
+  //           var email = $("input#email").val();
+  //           var message = $("textarea#message").val();
+  //
+  //           if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
+  //               alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
+  //           }
+  //           else if($("input#name").val() && $("input#email").val()){
+  //               alert("Thank you for reaching ou to us. If you wish to leave a message please type in on the message section..");
+  //           }
+  //          else{
+  //            alert("Please provide correct name or email")
+  //          }
+  //       });
+  // });
+  $(document).ready(function() {
+      $("#contactform").submit(function(event) {
+          var name = $("input#name").val();
+          var email = $("input#email").val();
+          var message = $("input#message").val();
+          $("#name").text(nameInput);
+          $("#email").text(emailInput);
+          $("#message").text(messageInput);
 
-            if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
-                alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
-            }
-            else if($("input#name").val() && $("input#email").val()){
-                alert("Thank you for reaching ou to us. If you wish to leave a message please type in on the message section..");
-            }
-           else{
-             alert("Please provide correct name or email")
-           }
-        });
+          alert("Email well received");
+
+          event.preventDefault();
+      });
   });
